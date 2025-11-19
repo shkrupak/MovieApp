bb2fac50500e0e3716549cd96cb57d35ded9f8b8
Rupak Shakya <shk.rupak@gmail.com>
2025-11-19 17:08:08 +0545

    initial commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Rupak Shakya on 19/11/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
